fb8c52beb0aa859be93e2bd3150235d862755b6d
Jim Pick <jim@jimpick.com>
2009-01-20 15:23:23 -0800

    Add flash messages, fix login links, set home page.
    Just a bunch of miscellaneous fixes.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
       redirect_back_or_default('/')
       flash[:notice] = "Logged in successfully"
     else
+      flash.now[:error] = "Authentication failed."
       render :action => 'new'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ ActionController::Routing::Routes.draw do |map|
   #map.resources(:posts)
   #map.resources(:comments, :path_prefix => '/posts/:post_id')
 
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  # map.root :controller => "welcome"
+  map.root :controller => "posts"
+
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'

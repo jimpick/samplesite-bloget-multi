835972fbfa3a8bf4575ee37397631030dcb9d89c
Jim Pick <jim@jimpick.com>
2009-01-19 19:31:50 -0800

    Fork for multisite development.
    Also add /signup, /login, /logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource  :session
-  
+
+  map.signup  '/signup', :controller => 'users',   :action => 'new' 
+  map.login  '/login',  :controller => 'session', :action => 'new'
+  map.logout '/logout', :controller => 'session', :action => 'destroy'
+ 
   map.with_options :controller => 'posts', :conditions => {:method => :get} do |bloget|
     bloget.blog_url '/blog', :action => 'index'
     bloget.formatted_blog_url '/blog.:format', :action => 'index'
